19b0da437017c1a3ee25511b567633644bbba6f2
gammar07 <marcin.gamza@gmail.com>
2018-08-22 14:28:48 +0200

    Adding flask transcation endpoint

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,10 @@
 import hashlib
 import json
+from textwrap import dedent
 from time import time
+from uuid import uuid4
+
+from flask import Flask
 
 
 class Blockchain(object):
@@ -106,3 +110,43 @@ class Blockchain(object):
     def last_block(self):
         # Returns last block in the chain
         return self.chain[-1]
+
+
+
+app = Flask(__name__)
+
+# Generate a globally unique address for this node
+node_identifier = str(uuid4()).replace('-','')
+
+blockchain = Blockchain()
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    return "We'll mine new block"
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    values = request.get_json()
+
+    #Check that the fields are in the POST data
+    required = ['sender', 'recipient', 'amount']
+    if not all (k in values for k in required):
+        return 'Missing values', 400
+
+    # Create new transaction
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+    response = {'message': f'Transaction wil be added to Block {index}'}
+    return jsonify(response), 201
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chian': blockchain.chain,
+        'length': len(blockchain.chain)
+    }
+    return jsonify(response), 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
